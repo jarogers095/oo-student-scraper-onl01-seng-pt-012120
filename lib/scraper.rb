cff4543fec15284eef65af6210634f112ca56b58
James Rogers <jarogers095@gmail.com>
2020-02-26 03:12:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,12 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     page = Nokogiri::HTML(open(index_url))
-    return page.css(".")
+    cards = page.css(".student-card")
+    students_array = []
+    cards.each do |card|
+      student_hash = {
+        name: card.css()
+      }
   end
 
   def self.scrape_profile_page(profile_url)
