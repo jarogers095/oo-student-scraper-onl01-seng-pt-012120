708dc4b4f962480429a4be0c9ddf0cbf33ac9e4f
James Rogers <jarogers095@gmail.com>
2020-02-26 03:07:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     page = Nokogiri::HTML(open(index_url))
-    return page.css()
+    return page.css("")
   end
 
   def self.scrape_profile_page(profile_url)
